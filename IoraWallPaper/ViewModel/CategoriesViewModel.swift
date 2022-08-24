d615cba9a5c6b1b426863044a7219ee63e568f26
wargi <wkahdla12346@gmail.com>
2022-08-24 15:36:45 +0900

    Etc: CategoriesViewModel 추가

--- IoraWallPaper/ViewModel/CategoriesViewModel.swift
+++ IoraWallPaper/ViewModel/CategoriesViewModel.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+final class CategoriesViewModel: ObservableObject {
+    @Published var categories: [Tag]
+    
+    init() {
+        self.categories = WallPapers.shared.tags
+        
+        let a = Tag(id: UUID(), info: TagInfo(name: "Winter", desc: "How do you overcome the servere cold weather?"), result: [])
+        let b = Tag(id: UUID(), info: TagInfo(name: "Fall", desc: "Let\'s go leat peeping."), result: [])
+        let c = Tag(id: UUID(), info: TagInfo(name: "Summer", desc: "Is there any way to beat the heat?"), result: [])
+        let d = Tag(id: UUID(), info: TagInfo(name: "Spring", desc: "Spring is already in the air."), result: [])
+        
+        categories.append(contentsOf: [a, b, c, d])
+    }
+}
